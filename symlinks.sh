e9e3f2fe8198a1b46d4038c3d78e0f2de37d73f8
Ganoodles <swajabbadaja@gmail.com>
2024-08-10 02:03:28 -0700

    first commit

---
+++ symlinks.sh
@@ -0,0 +1,10 @@
+#hyprland
+mkdir ~/.config/hypr
+ln -s ~/dotfiles/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+mkdir ~/.config/hypr/themes
+ln -s ~/dotfiles/hyprland/themes/mocha.conf ~/.config/hypr/themes/mocha.conf
+
+#waybar
+mkdir ~/.config/waybar/
+ln -s ~/dotfiles/waybar/config ~/.config/waybar/config
+ln -s ~/dotfiles/waybar/style.css ~/.config/waybar/style.css
